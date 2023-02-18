5b28a92ad95e743c41b1c6f170be091ec620e755
adamali90 <riktechsolutions@gmail.com>
2023-02-19 00:27:34 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+
+import logo from "./mycashlogosml.png"
+function App() {
+  return (
+    <div className="bg-gray-200">
+      <div className=" border-b border-black-200">
+        <nav className="grid-cols-1">
+          <div className=" flex justify-start">
+            <img src={logo} alt="MycashPolicy"/>
+          </div> 
+          <ul>
+            <li className="flex justify-center -mt-20  ">
+               <h1 className="text-4xl font-bold " >Amtel Mycash Policy</h1>
+            </li>
+          </ul>
+          <div className="  flex justify-end text-blue-700 -mt-20 ">
+              <a href="https://amtelkom.com">
+                <span >Go to Amtel Website</span>
+              </a>
+          </div>
+        </nav>
+      </div>
+      <div className="font-body mt-40">
+        <p className="mb-10 mr-10 ml-10 border border-gray-200">
+        <h1 className="text-xl font-bold underline flex justify-center">Privacy Policy</h1>
+        This privacy policy sets out how Amtelkom uses and protects any information that Amtelkom have about you when you use our website or mobile application or otherwise provide your personal details to us.
+Amtelkom is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using our Service (as defined below), then you can be assured that it will only be used in accordance with this privacy policy.
+Amtelkom may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.
+
+<h1 className="text-xl font-bold underline flex justify-center">Who is Amtelkom?</h1>
+Amal Group has identified opportunity to enter the telecommunications market which would complement the Group's businesses of that time, The entry to the market has accelerated by acquisition of Africanonline / Orbit("AFORB") business, which provided a basic network that helped to use to quickly launch Amtel's operations. This in turn gave a hand to the licensing of Amal’s special entry for telecommunications, Amtel Ltd.
+
+Amtel is headquartered in Mogadishu with other main regional offices. And from humble beginnings, Amtelkom started in 2016 as a telecommunication company and has grown to expand its network through Somalia and still is expanding, it is a purpose-driven company in the Horn of Africa.
+
+Amtelkom operations include telecommunications, mobile money, internet and other key sectors providing efficient, trusted, and low-cost services. Amtelkom also provides a critical economic and transformative lifeline to the Somali population. 
+
+<h1 className="text-xl font-bold underline flex justify-center">The Policy</h1>
+
+AMTEL LTD built the MyCash app as a Free app. This SERVICE is provided by AMTEL LTD at no cost and is intended for use as is. 
+This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use MyCashService. 
+If you choose to use MyCash Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy. 
+The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at terms and conditions of use of the app is needed  unless otherwise defined in this Privacy Policy. 
+Information Collection and Use
+For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information. The information that werequest will be retained by us and used as described in this privacy policy. 
+The app does use third party services that may collect information used to identify you.
+Link to privacy policy of third party service providers used by the app
+Google Play Services
+Log Data
+We want to inform you that whenever you use MyCash Service, in a case of an error in the app we collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing MyCash Service, the time and date of your use of the Service, and other statistics. 
+
+<h1 className="text-xl font-bold underline flex justify-center">Cookies</h1>
+Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory. 
+This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse these cookies, you may not be able to use some portions of this Service. 
+Service Providers
+We may employ third-party companies and individuals due to the following reasons:
+To facilitate our Service;
+To provide the Service on our behalf;
+To perform Service-related services; or
+To assist us in analyzing how our Service is used.
+We want to inform users of MyCash Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose. 
+Security
+We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security. 
+
+<h1 className="text-xl font-bold underline flex justify-center">Links to other Sites</h1>
+This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by AMTEL LTD. Therefore, westrongly advise you to review the Privacy Policy of these websites. AMTEL LTD has no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services. 
+
+<h1 className="text-xl font-bold underline flex justify-center">Future Policy Changes</h1>
+We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page. 
+<h1 className="text-xl font-bold underline flex justify-center">Contact Us</h1>
+If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at MyCashso.so or MyCashso.com or Amtelkom.com
+        </p>
+      </div>
+      <div className="footer">
+        
+
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
